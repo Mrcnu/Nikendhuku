20803b6573fd666b8f7e6558949ff8f8cef92a1b
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-07-27 21:10:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,8 +65,8 @@ SHORT_API = environ.get("SHORT_API")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001812524383'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Muvies_Mawa')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "False"), False)
 IMDB = is_enabled(environ.get('IMDB', "False"), False)
